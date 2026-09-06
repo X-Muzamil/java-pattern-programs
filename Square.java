e5e3116d2188ce089c753454399babb57825e6ec
Muzamilcsdev <soomromuzamil096@gmail.com>
2026-09-06 06:22:55 +0500

    done with square pattern logic

---
+++ Square.java
@@ -0,0 +1,11 @@
+class Square{
+    public static void square(){
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                System.out.print("*");
+            }
+                System.out.println();
+        }
+
+    }
+}
