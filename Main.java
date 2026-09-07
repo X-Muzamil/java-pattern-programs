20639054a72efa760c1e6d2ad450d3433966586a
Muzamilcsdev <soomromuzamil096@gmail.com>
2026-09-07 06:21:42 +0500

    right angle traingel logic

--- Main.java
+++ Main.java
@@ -1,5 +1,6 @@
 class Main{
     public static void main(String args[]){
         Square.square();
+        Square.rightangeTriangle();
     }
 }
--- Square.java
+++ Square.java
@@ -1,10 +1,23 @@
 class Square{
     public static void square(){
+        int n = 5;
+
         for(int i=0;i<n;i++){
             for(int j=0;j<n;j++){
                 System.out.print("*");
             }
                 System.out.println();
+        }
+
+    }
+
+    public static void rightangeTriangle(){
+        int n = 8;
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
         }
 
     }
