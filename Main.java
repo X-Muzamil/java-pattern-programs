25c085643415d4a05eaf004aba2f91f70421ecf5
Muzamilcsdev <soomromuzamil096@gmail.com>
2026-09-07 06:37:38 +0500

    Diamond and paramid

--- Main.java
+++ Main.java
@@ -2,5 +2,9 @@ class Main{
     public static void main(String args[]){
         Square.square();
         Triangle.rightangeTriangle();
+        Diamond.paramid();
+        Diamond.diamond();
     }
+
+
 }
